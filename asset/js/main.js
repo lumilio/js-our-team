0a628b49460b1d3fb0d1ca9e78fd8216088fa524
lumilio <logistic-division@lumilium.com>
2021-10-25 09:21:19 +0200

    aggiungo un arrey di oggetti

---
+++ asset/js/main.js
@@ -0,0 +1,33 @@
+const team = [
+
+    {
+        name: "Lorenzo",
+        role: "Student",
+        image: ""
+    },
+
+    {
+        name: "Fabio",
+        role: "Teacher",
+        image: ""
+    },
+
+    {
+        name: "Mark Z",
+        role: "CEO",
+        image: ""
+    },
+
+    {
+        name: "Fabiola",
+        role: "Teacher",
+        image: ""
+    },
+
+    {
+        name: "Tiziano",
+        role: "Teacher",
+        image: ""
+    }
+
+]
