13ee09242ffe2d8a8c97945edfce8ef471588aeb
lumilio <logistic-division@lumilium.com>
2021-10-25 09:52:32 +0200

    #

--- asset/js/main.js
+++ asset/js/main.js
@@ -34,6 +34,7 @@ const team = [
 
 for (let index = 0; index < team.length; index++) {
     const member = team[index];
+    console.log(member);
     console.log('name: ', member.name);
     console.log('role: ', member.role);
     console.log('image: ', member.image);
